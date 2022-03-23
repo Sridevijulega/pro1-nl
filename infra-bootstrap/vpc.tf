74b31fbcd922c25527fde92cf065e40178a43cc1
sridevi julega <srridevijulega@gmail.com>
2022-03-23 21:58:25 +0530

    add vpc

---
+++ infra-bootstrap/vpc.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "~>2.6.0"
+
+  name = "pronl-app-${var.app_environment}"
+  cidr = "10.0.0.0/16"
+  azs = var.azs
+  public_subnets = [
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24"]
+  enable_dns_hostnames = true
+
+  tags = {
+    project = "pronl-app"
+  }
+}
